c7b680e2503df461d876e9380f3b680c06219af3
Aqib-Malik <hmaqib92@mail.com>
2022-10-19 09:42:25 +0500

    filters added

---
+++ filtration/filters.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
+
+namespace EMS.filtration
+{
+    [Route("filter")]
+    public class MoviesController : Controller
+    {
+        private readonly IActionResult service;
+
+        public MoviesController(IActionResult service)
+        {
+            this.service = service;
+        }
+
+        [HttpGet(Name = "GetMovies")]
+        public IActionResult Get(FilteringParams filteringParams)
+        {
+            var model = service.Getemp(filteringParams);
+
+            var outputModel = new empOutputModel
+            {
+                Count = model.Count,
+                Items = model.Select(m => ToempInfo(m)).ToList(),
+            };
+            return Ok(outputModel);
+        }
+    }
+}
